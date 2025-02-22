8eada1ea687d47112730a4ee121085fc482c1454
lcs-nhwang <sjnhwang@lcs.on.ca>
2025-02-22 11:05:52 -0500

    Initial Commit

---
+++ StepperExample/StepperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExampleApp.swift
+//  StepperExample
+//
+//  Created by Nicholas Hwang on 22/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct StepperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
